fdd17b6483cf125148a9dc83f7a90f159d952bef
Zetta <zinv@coinmeca.net>
2023-07-04 18:48:57 +0900

    test

--- src/components/layouts/menu/Menu.tsx
+++ src/components/layouts/menu/Menu.tsx
@@ -41,7 +41,7 @@ export default function Menu(props: Menu) {
             <Style $scale={scale}>
                 {typeof props?.menu !== "string" && props?.menu?.length > 0 ? 
                     (
-                        props?.menu?.map((menu: any, i: number) => menu?.children ? <div key={i} data-index={i} data-menu={menu?.children} /> : <div key={i} data-index={i} data-menu={menu} />)
+                        props?.menu?.map((menu: any, i: number) => {return menu?.children ? <div key={i} data-index={i} data-menu={menu?.children} /> : <div key={i} data-index={i} data-menu={menu} />})
                     ) : (
                         <Row $scale={scale}>{props?.menu}</Row>
                         )
