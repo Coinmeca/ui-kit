c4681aac813754f3176befca767059c06de26a80
Zetta <zinv@coinmeca.net>
2023-07-05 00:52:11 +0900

    Update - Style

--- src/components/controls/tab/Tab.tsx
+++ src/components/controls/tab/Tab.tsx
@@ -25,6 +25,7 @@ export default function Tab(props: Tab) {
     const fit = props?.fit || false;
 
     const onClick = (e: any) => {
+        if (props?.disabled) return;
         if (typeof props?.onClick === "function") props?.onClick(e);
     };
 
