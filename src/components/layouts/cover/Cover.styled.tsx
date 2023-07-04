b0f5d665934cffa7b3cb7ca58040cc3f60f88a7d
Zetta <zinv@coinmeca.net>
2023-07-04 14:31:01 +0900

    Update - Menu

--- src/components/layouts/cover/Cover.styled.tsx
+++ src/components/layouts/cover/Cover.styled.tsx
@@ -1,5 +1,8 @@
 import { styled } from "styled-components";
 
-const Style = styled.div<{ $fullsize: boolean }>``;
+const Style = styled.div<{ $height:number; $fullsize?: boolean }>`
+    width: ${({$fullsize, $height}) => $fullsize ? '100vw' : `100%`};
+    height: ${({$fullsize, $height}) => $fullsize ? '100vh' : `${$height}px`}
+`;
 
 export default Style;
--- src/components/layouts/cover/Cover.tsx
+++ src/components/layouts/cover/Cover.tsx
@@ -1,11 +1,21 @@
-import type { BG } from "components/layouts/bg/BG";
+import type { BG as Background } from "components/layouts/bg/BG";
+import { BG } from "components/layouts";
+import Style from "./Cover.styled";
 
-export interface Cover {
+interface Cover {
     children?: any;
-    background?: BG;
+    background?: Background;
+    height?:number;
     fullsize?: boolean;
 }
 
 export default function Cover(props: Cover) {
-    return <>{props?.children}</>;
+    const height = props?.height || 320;
+
+    return (
+        <Style $height={height} $fullsize={props?.fullsize}>
+            {props?.background && <BG {...props?.background} />}
+            {props?.children}
+        </Style>
+    );
 }
