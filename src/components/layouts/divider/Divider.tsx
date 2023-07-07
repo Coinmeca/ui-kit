9f97c313e72c87975a82cfd8a3f9664568e41e0b
ZINV <71391363+l-Zetta-l@users.noreply.github.com>
2023-07-07 17:18:49 +0900

    Update - Divider

--- src/components/layouts/divider/Divider.tsx
+++ src/components/layouts/divider/Divider.tsx
@@ -4,11 +4,27 @@ export interface Divider {
     vertical?: boolean;
     margin?: number;
     style?: object;
+    children?: any;
+    align?: "left" | "right";
+    gap?: number;
 }
 
 export default function Divider(props: Divider) {
     const vertical = props?.vertical || false;
     const margin = props?.margin || 0;
+    const gap = props?.gap || 2;
 
-    return <Style $vertical={vertical} $margin={margin} style={props?.style} />;
+    return (
+        <Style $vertical={vertical} $margin={margin} $gap={gap} style={props?.style}>
+            {props?.children ? (
+                <>
+                    {props?.align !== "right" && <div />}
+                    <span>{props?.children}</span>
+                    {props?.align !== "left" && <div />}
+                </>
+            ) : (
+                <div />
+            )}
+        </Style>
+    );
 }
