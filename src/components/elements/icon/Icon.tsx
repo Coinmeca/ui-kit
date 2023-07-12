cb4a2c9c99f36df77641ff7ddaaf587c3f5a0cd0
ZINV <71391363+l-Zetta-l@users.noreply.github.com>
2023-07-13 06:56:53 +0900

    Test - Icon

--- src/components/elements/icon/Icon.tsx
+++ src/components/elements/icon/Icon.tsx
@@ -21,7 +21,7 @@ function Icon(props: Icon) {
     return (
         <Style title={title} $color={color} $scale={scale}>
             <>
-                {Icons}
+                <Icons />
                 {count && count > 0 && <Count $color={color}>{count}</Count>}
             </>
         </Style>
