04934316eb9c5ce29337ce469ac936d377bb0bf6
ZINV <71391363+l-Zetta-l@users.noreply.github.com>
2023-07-13 06:05:33 +0900

    Re update - app and document

---
+++ src/app/pages/_app.tsx
@@ -0,0 +1,15 @@
+import "./globals.scss";
+import { ThemeProvider } from "styled-components";
+import { Style } from "lib";
+
+export default function App({ Component, pageProps }: { Component: any; pageProps: any }) {
+    return (
+        <>
+            <Style.Initialize>
+                {/* <ThemeProvider theme={theme}> */}
+                <Component suppressHydrationWarning={true} {...pageProps} />
+                {/* </ThemeProvider> */}
+            </Style.Initialize>
+        </>
+    );
+}
