7f3d43b5c35429a0d19810b0845bacaf69c3022a
ZINV-crossangle <zinv@coinmeca.net>
2023-07-13 18:19:39 +0900

    Update - Orderbook

---
+++ src/app/samples/layout.tsx
@@ -0,0 +1,20 @@
+"use client";
+import { Frames } from "components";
+import type { Frame } from "components/frames/Frame";
+import { Style } from "lib";
+import Dummy from "./dummy";
+
+export interface Exchange {
+    frames?: Frame;
+    children?: any;
+}
+
+export default function Layout(props: Exchange) {
+    const { header, sidebars } = Dummy();
+
+    return (
+        <Frames.Frame header={header} sidebar={sidebars} align={"right"} background={{ img: { src: 2 } }} side={56}>
+            {props?.children}
+        </Frames.Frame>
+    );
+}
---
+++ src/parts/orderbook/Orderbook.tsx
@@ -0,0 +1,14 @@
+﻿export interface Orderbook{
+    asks?: Tick[];
+    bids?: Tick[];
+}
+
+export interface Tick{
+    price: number | string;
+    balance: number | string;
+}
+
+export default function Ordrebook(props: Orderbook) {
+
+    return <></>
+}
