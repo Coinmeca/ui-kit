9154490e4c919b74c707d009bd3afb8227fd167e
ZINV <71391363+l-Zetta-l@users.noreply.github.com>
2023-07-18 03:06:13 +0900

    Testing Input

--- src/components/frames/Frame.styled.tsx
+++ src/components/frames/Frame.styled.tsx
@@ -7,8 +7,8 @@ const Style = styled.section`
     position: relative;
     display: flex;
     flex-direction: column;
-    width: 100%;
-    height: 100%;
+    width: 100vw;
+    height: 100vh;
     overflow: hidden;
     z-index: 1;
 
