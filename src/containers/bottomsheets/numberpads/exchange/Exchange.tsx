24d8cf3b2e924e59f2a31d6a3b17903841e56dc0
ZINV <71391363+l-Zetta-l@users.noreply.github.com>
2023-07-17 22:36:01 +0900

    Update - bottomsheet numberpad

--- src/containers/bottomsheets/numberpads/exchange/Exchange.tsx
+++ src/containers/bottomsheets/numberpads/exchange/Exchange.tsx
@@ -4,9 +4,13 @@ import { BottomSheet } from "containers";
 import { Numberpads } from "parts";
 import type { BottomSheet as Sheet } from "containers/bottomsheets/BottomSheet";
 import type { Numberpad as Pad } from "parts/numberpads/Numberpad";
+import { useEffect, useState } from "react";
 
 export interface Exchange extends Pad, Sheet {
+    label?: string;
+    placeholder?: string;
     padding?: number;
+    onChange?: Function;
 }
 
 export default function Exchange(props: Exchange) {
@@ -15,10 +19,21 @@ export default function Exchange(props: Exchange) {
     const height = { min: (typeof props?.height === "object" ? props?.height?.min : props?.height) || min, max: (typeof props?.height === "object" ? props?.height?.min : props?.height) || max };
     const padding = props?.padding || 2;
 
+    const [value, setValue] = useState(props?.value || "");
+
+    useEffect(() => {
+        if (props?.value) setValue(props?.value?.toString() || "");
+    }, [props?.value]);
+
+    const onChange = (e: any, v: string) => {
+        if (typeof props?.onChange === "function") props?.onChange(e, v);
+        setValue(v);
+    };
+
     return (
         <BottomSheet active={props?.active} height={height}>
             <Layouts.Col gap={0}>
-                <Layouts.Row gap={1} style={{ alignItems: "center", padding: `${padding / 2}em ${padding}em` }}>
+                <Layouts.Row gap={1} style={{ alignItems: "center", padding: `${padding / 2}em` }}>
                     <Elements.Text type={"desc"} weight={"bold"} align={"right"}>
                         = 1.9234234
                     </Elements.Text>
@@ -26,10 +41,17 @@ export default function Exchange(props: Exchange) {
                         ETH
                     </Elements.Text>
                 </Layouts.Row>
-                <div>
-                    <Controls.Input style={{ padding: `${padding / 8}em ${padding * 1.5}em` }} unit={"ETH"} />
-                </div>
-                <Numberpads.Exchange {...props} padding={padding} />
+                <Controls.Input
+                    value={value}
+                    onChange={(e: any, v: string) => onChange(e, v)}
+                    placeholder={props?.placeholder}
+                    type={"currency"}
+                    align={"right"}
+                    left={{ children: <span>Price</span> }}
+                    style={{ padding: `${padding / 8}em ${padding}em` }}
+                    unit={"ETH"}
+                />
+                <Numberpads.Exchange {...props} value={value} onChange={(e: any, v: string) => onChange(e, v)} padding={padding} reverse={false} />
             </Layouts.Col>
         </BottomSheet>
     );
