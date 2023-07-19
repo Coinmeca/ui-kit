ffec5fd03483d5fb9d3fd7a8cf30a46da47f619a
ZINV <71391363+l-Zetta-l@users.noreply.github.com>
2023-07-20 06:42:12 +0900

    Update - Bottomsheets Max heights

--- src/containers/bottomsheets/BottomSheet.styled.tsx
+++ src/containers/bottomsheets/BottomSheet.styled.tsx
@@ -1,21 +1,21 @@
 import { styled } from "styled-components";
 
-const Style = styled.div<{ $scale: number; $active: boolean; $height?: number | { min?: number; max?: number } }>`
+const Style = styled.div<{ $scale: number; $active: boolean; $height?: number | string | { min?: number | string; max?: number | string } }>`
     font-size: calc(var(--unit) * ${({ $scale }) => $scale});
     background: rgba(var(--black), var(--o09));
     position: absolute;
     bottom: 0;
     display: flex;
     width: -webkit-fill-available;
-    height: ${({ $height }) => (typeof $height === "number" && `${$height}em`) || "max-content"};
+    height: ${({ $height }) => (typeof $height === "number" ? `${$height}em` : $height === "string" && `${$height}`) || "max-content"};
     margin: 0 !important;
     text-align: center;
     pointer-events: ${({ $active }) => ($active ? "initial" : "none")};
     transform: translateY(${({ $active }) => ($active ? "0" : "100%")});
     transition: 0.3s ease;
 
-    ${({ $height }) => typeof $height === "object" && typeof $height?.min === "number" && `min-height: ${$height?.min}em`};
-    ${({ $height }) => typeof $height === "object" && typeof $height?.max === "number" && `max-height: ${$height?.max}em`};
+    ${({ $height }) => (typeof $height === "object" && typeof $height?.min === "number" ? `min-height: ${$height?.min}em` : typeof $height?.min === "string" && `min-height: ${$height?.min}`)};
+    ${({ $height }) => (typeof $height === "object" && typeof $height?.max === "number" ? `max-height: ${$height?.max}em` : typeof $height?.max === "string" && `max-height: ${$height?.max}`)};
 
     input {
         color: white;
