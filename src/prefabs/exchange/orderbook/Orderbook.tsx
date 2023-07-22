c207c701d1be85b3cdff574ebc2533b7053ce301
ZINV <71391363+l-Zetta-l@users.noreply.github.com>
2023-07-22 12:59:31 +0900

    Update - Numberpad Bottomsheets

--- src/prefabs/exchange/orderbook/Orderbook.tsx
+++ src/prefabs/exchange/orderbook/Orderbook.tsx
@@ -1,5 +1,8 @@
-﻿import { Layouts } from "components";
-import { Format } from "lib/utils";
+﻿"use client";
+import { Layouts } from "components";
+import { motion } from "framer-motion";
+import { Format, Sort } from "lib/utils";
+import { useState } from "react";
 import Style, { Asks, Bids, Tick as Ticks } from "./Orderbook.styled";
 
 export interface Orderbook {
@@ -21,27 +24,37 @@ export interface Tick {
 }
 
 export default function Ordrebook(props: Orderbook) {
-    const asks = props?.asks;
+    // const asks = props?.asks;
     const bids = props?.bids;
+    const [asks, setAsks] = useState<Tick[] | undefined>(props?.asks);
 
-    const ask_max: number = (asks && asks?.length > 0 && Math.max(...asks?.map((o: Tick) => parseFloat(o?.balance.toString())))) || 0;
-    const bid_max: number = (bids && bids?.length > 0 && Math.max(...bids?.map((o: Tick) => parseFloat(o?.balance.toString())))) || 0;
+    const ask_max: number = (asks && asks?.length > 0 && Math.max(...asks?.map((o: Tick) => parseFloat(o?.balance?.toString())))) || 0;
+    const bid_max: number = (bids && bids?.length > 0 && Math.max(...bids?.map((o: Tick) => parseFloat(o?.balance?.toString())))) || 0;
 
     const view = props?.view || 0;
 
     const handleAsk = (ask: Tick, e?: any) => {
+        setAsks((state: any) =>
+            Sort(
+                state.filter((s: Tick, i: number) => s?.price !== ask.price),
+                "price",
+                "number",
+                true
+            )
+        );
         if (typeof props?.onClickAsk === "function") props?.onClickAsk(ask, e);
     };
     const handleBid = (bid: Tick, e?: any) => {
+        setAsks((state: any) => Sort([...state, ...[asks && asks]], "price", "number", true));
         if (typeof props?.onClickBid === "function") props?.onClickBid(bid, e);
     };
 
     return (
         <Style $responsive={props?.responsive}>
-            <Asks $show={view === 0 || view === 1}>
+            <Asks $show={view === 0 || view === 1} as={motion.div}>
                 {asks && asks?.length > 0 ? (
                     asks?.map((ask: Tick, i: number) => (
-                        <Ticks key={i} onClick={(e: any) => handleAsk(ask, e)}>
+                        <Ticks key={i} onClick={(e: any) => handleAsk(ask, e)} as={motion.div} initial={{ opacity: 0 }} animate={{ opacity: 1 }} exit={{ opacity: 0 }} transition={{ duration: 0.3 }}>
                             <div>
                                 <div>
                                     <div>
