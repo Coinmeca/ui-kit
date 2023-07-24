3e9fee2578895a4fbb7d2cd496202d81782594ac
Zetta <zinv@coinmeca.net>
2023-07-24 20:38:48 +0900

    Fix - Errors

--- src/components/contents/states/success/Success.tsx
+++ src/components/contents/states/success/Success.tsx
@@ -1,9 +1,9 @@
 "use client";
-import { State } from "components/contents";
+import State from "components/contents/states/State";
 import type { State as Props } from "components/contents/states/State";
 
 export default function Success(props: Props) {
-    const img = props?.img?.src || require("/src/assets/icons/animation/icon_success.gif")?.default?.src;
+    const img = props?.img?.src || require("/src/assets/icons/animation/icon_success.gif");
 
     return <State {...props} img={{ src: img }} />;
 }
