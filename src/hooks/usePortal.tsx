8bed8d31bd8f7baf7e13fc41a4da14f1e58ad643
ZINV-crossangle <zinv@coinmeca.net>
2023-07-27 17:28:20 +0900

    Update - usePortal

--- src/hooks/usePortal.tsx
+++ src/hooks/usePortal.tsx
@@ -50,23 +50,3 @@ export default function usePortal(initial?: any, initialProps?: any) {
         () => setActive(false),
     ];
 }
-
-// "use client";
-// import { useEffect, useState } from "react";
-// import { createPortal } from "react-dom";
-// import { Root, createRoot } from "react-dom/client";
-
-// export default function usePortal(children: any, props?: any) {
-//     const [root, setRoot] = useState<Root | undefined>();
-//     const [active, setActive] = useState(false);
-
-//     useEffect(() => {
-//         !root && setRoot(createRoot(document?.createElement("section")));
-//     }, []);
-
-//     useEffect(() => {
-//         root?.render(active ? createPortal(typeof children === "function" && props ? children(props) : children, document?.body) : null);
-//     }, [root, children, active]);
-
-//     return [() => setActive(true), () => setActive(false)];
-// };
