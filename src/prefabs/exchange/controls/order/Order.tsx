e6c3a2683bbd15a2f7082bb0e7933067d3102dfd
ZINV-crossangle <zinv@coinmeca.net>
2023-07-27 17:27:02 +0900

    Change - Order Bottomsheet

--- src/prefabs/exchange/controls/order/Order.tsx
+++ src/prefabs/exchange/controls/order/Order.tsx
@@ -187,8 +187,7 @@ export default function Order(props: OrderControl) {
                 value={currency === 0 ? order?.quantity : order?.amount}
                 max={currency === 0 ? (order?.quantity || 1) / order.price : order.amount}
                 onChange={(e: any, v: any) => handleChangeAmount(v)}
-                // onClick={() => isMobile && handleAmountPad()}
-                onClick={() => handleAmountPad()}
+                onClick={() => isMobile && handleAmountPad()}
                 inputMode={isMobile ? "none" : undefined}
                 left={{ children: <span>{currency === 0 ? "Qunatity" : "Amount"}</span> }}
                 right={{
