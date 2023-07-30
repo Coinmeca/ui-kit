9d0ae4512a628fc0211433cbee41bd027a8fc60b
Dobby <git@dobe.st>
2023-07-31 01:59:52 +0900

    feat: contracts

---
+++ src/contracts/instances/index.ts
@@ -0,0 +1,7 @@
+import { getContract } from "../factories";
+
+export const App = getContract("App");
+export const Farm = getContract("Farm");
+export const Market = getContract("Market");
+export const Orderbook = getContract("Orderbook");
+export const Vault = getContract("Vault");
