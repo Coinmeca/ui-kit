15fadad4dcea9170f5b3e06279d7b55ce3a7c464
ZINV <71391363+l-Zetta-l@users.noreply.github.com>
2023-08-02 23:50:04 +0900

    Composite - Monorepo

--- src/components/contents/states/success/Success.tsx
+++ src/components/contents/states/success/Success.tsx
@@ -3,7 +3,7 @@ import State from "components/contents/states/State";
 import type { State as Props } from "components/contents/states/State";
 
 export default function Success(props: Props) {
-    const img = props?.img?.src || require("/src/assets/animation/success.gif");
+    const img = props?.img?.src || require("../../../../assets/animation/success.gif");
 
     return <State {...props} img={{ src: img }} />;
 }
