35c8d3e84a7af385cc8ac686c34919ec88e6c740
ZINV <71391363+l-Zetta-l@users.noreply.github.com>
2023-08-02 23:51:04 +0900

    Update - Package/UI

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,7 @@
-<<<<<<< HEAD
-export * as Component from 'components';
-export * as Containers from 'containers';
-export * as Parts from 'parts';
-export * as Prefabs from 'prefabs';
-export * as Hooks from 'hooks';
-export * as Libs from 'lib';
-=======
 export * as Component from './components';
 export * as Containers from './containers';
 export * as Parts from './parts';
 export * as Prefabs from './prefabs';
 export * as Hooks from './hooks';
 export * as Libs from './lib';
->>>>>>> 98412a9971cb146c0467307fbedd10c83ea5e6e3
 export * as Types from './types';
