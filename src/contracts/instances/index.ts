c9a192941ed057f49601cb9c4df75f0da7fe67c8
ZINV <71391363+l-Zetta-l@users.noreply.github.com>
2023-08-02 23:27:59 +0900

    Composite - Monorepo

--- src/contracts/instances/index.ts
+++
@@ -1,7 +0,0 @@
-import { getContract } from "../factories";
-
-export const App = getContract("App");
-export const Farm = getContract("Farm");
-export const Market = getContract("Market");
-export const Orderbook = getContract("Orderbook");
-export const Vault = getContract("Vault");
---
+++ tsup.config.ts
@@ -0,0 +1,16 @@
+import { defineConfig } from 'tsup';
+
+export default defineConfig((options: any) => ({
+  entry: ['src/index.ts'],
+  splitting: true,
+  treeshake: true,
+  clean: true,
+  dts: true,
+  format: ['esm'],
+  platform: 'browser',
+  target: ['es2020', 'chrome70', 'edge18', 'firefox70', 'node18'],
+  // tsconfig: new URL('./tsconfig.build.json', import.meta.url).pathname,
+  tsconfig: "./tsconfig.build.json",
+  sourcemap: !options.watch,
+  minify: !options.watch,
+}));
