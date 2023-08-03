74a6d5e660a2875933adfc57d3f6136ca91290c1
ZINV <71391363+l-Zetta-l@users.noreply.github.com>
2023-08-03 21:49:31 +0900

    Update - tsup setting

--- tsup.config.ts
+++ tsup.config.ts
@@ -1,12 +1,14 @@
 import { defineConfig } from 'tsup';
 
 export default defineConfig((options: any) => ({
-  entry: ['src/index.ts'],
+  entry: [
+    'src/index.ts'
+  ],
   splitting: true,
   treeshake: true,
   clean: true,
   dts: true,
-  format: ['esm'],
+  format: ["esm", "cjs"],
   platform: 'browser',
   target: ['es2020', 'chrome70', 'edge18', 'firefox70', 'node18'],
   // tsconfig: new URL('./tsconfig.build.json', import.meta.url).pathname,
