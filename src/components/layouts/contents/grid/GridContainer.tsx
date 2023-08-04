77d10fce5177b4320c7a0bb44a11c6fbb2a3b40a
Zetta <zinv@coinmeca.net>
2023-08-04 18:35:00 +0900

    Fix - SSR id Error

--- src/components/layouts/contents/grid/GridContainer.tsx
+++ src/components/layouts/contents/grid/GridContainer.tsx
@@ -27,43 +27,26 @@ export interface GridContainer {
     fullsize?: boolean;
 }
 
-const GridStyle = createGlobalStyle<{
-    $parent: string;
-    $id: number;
-    $area?: string;
-    $responsive?: {
-        device: "desktop" | "laptop" | "tablet" | "mobile";
-        area?: string;
-    }[];
-}>`
-    #${({ $parent }) => $parent} > *:nth-child(${({ $id }) => `${$id + 1}`}){
-        ${GridArea};  
-    }
-`;
-
 export default function GridContainer(props: GridContainer) {
     const gap = props?.gap || 4;
-    const grid = useId().replaceAll(":", "");
 
     return (
         <Style $fullsize={props?.fullsize || false}>
-            <Grid id={grid} $area={props?.area} $direction={props?.direction} $gap={gap} $width={props?.width} $height={props?.height} $responsive={props?.responsive} style={props?.style}>
+            <Grid $area={props?.area} $contents={(props?.contents && props?.contents?.length > 0) && props?.contents?.map((c: Content) => c.area) || []} $direction={props?.direction} $gap={gap} $width={props?.width} $height={props?.height} $responsive={props?.responsive} style={props?.style}>
                 <>
                     {props?.contents && props?.contents?.length > 0
                         ? props?.contents?.map((v: any, k: number) => (
-                              <Fragment key={k}>
-                                  <GridContent className={k} format={props?.format} {...v} />
-                                  <GridStyle {...{ $parent: grid, $id: k, $area: v?.area, $responsive: v?.responsive }} />
-                              </Fragment>
-                          ))
+                            <Fragment key={k}>
+                                <GridContent className={k} format={props?.format} {...v} />
+                            </Fragment>
+                        ))
                         : props?.children && props.children?.length > 0
-                        ? props?.children?.map((v: any, k: number) => (
-                              <Fragment key={k}>
-                                  <Fragment>{v}</Fragment>
-                                  <GridStyle {...{ $parent: grid, $id: k, $area: v?.area, $responsive: v?.responsive }} />
-                              </Fragment>
-                          ))
-                        : props?.children}
+                            ? props?.children?.map((v: any, k: number) => (
+                                <Fragment key={k}>
+                                    <Fragment>{v}</Fragment>
+                                </Fragment>
+                            ))
+                            : props?.children}
                 </>
             </Grid>
         </Style>
