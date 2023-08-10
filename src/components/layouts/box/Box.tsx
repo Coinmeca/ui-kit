1389a7982eb0228779af9d718079d6aa2f941cd7
Zetta <zinv@coinmeca.net>
2023-08-10 11:00:00 +0900

    Update - Box

--- src/components/layouts/box/Box.tsx
+++ src/components/layouts/box/Box.tsx
@@ -3,7 +3,7 @@ import Style from "./Box.styled";
 export interface Box {
     style?: object;
     children?: any;
-    padding?: number | number[];
+    padding?: number | any[];
     change?: string | false;
     fit?: boolean;
 }
@@ -25,11 +25,11 @@ export default function Box(props: Box) {
         typeof props?.padding === "number"
             ? { top: props?.padding, right: props?.padding, bottom: props?.padding, left: props?.padding }
             : {
-                  top: (props?.padding && typeof props?.padding[0] !== "string" && props?.padding[0]) || initial,
-                  right: (props?.padding && (init(props?.padding[1]) || init(props?.padding[0]))) || initial,
-                  bottom: (props?.padding && (init(props?.padding[2]) || init(props?.padding[0]))) || initial,
-                  left: (props?.padding && (init(props?.padding[3]) || init(props?.padding[1]) || init(props?.padding[0]))) || initial,
-              };
+                top: (props?.padding && typeof props?.padding[0] !== "string" && props?.padding[0]) || initial,
+                right: (props?.padding && (init(props?.padding[1]) || init(props?.padding[0]))) || initial,
+                bottom: (props?.padding && (init(props?.padding[2]) || init(props?.padding[0]))) || initial,
+                left: (props?.padding && (init(props?.padding[3]) || init(props?.padding[1]) || init(props?.padding[0]))) || initial,
+            };
 
     return (
         <Style $change={props?.change || undefined} $padding={padding} $fit={props?.fit} style={props?.style}>
