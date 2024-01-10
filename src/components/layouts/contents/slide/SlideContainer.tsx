164ffea5508a018d8a206117e7f39838a585208d
ZINV <71391363+l-Zetta-l@users.noreply.github.com>
2024-01-11 02:08:06 +0900

    Update - Input Test

--- src/components/layouts/contents/slide/SlideContainer.tsx
+++ src/components/layouts/contents/slide/SlideContainer.tsx
@@ -1,25 +1,28 @@
-import SlideContent from './SlideContent';
-import type {SlideContent as Content} from './SlideContent';
-import Style from './SlideContainer.styled';
+import SlideContent from "./SlideContent";
+import type { SlideContent as Content } from "./SlideContent";
+import Style from "./SlideContainer.styled";
+import { memo } from "react";
 
 export interface SlideContainer {
-	contents?: Content[];
-	style?: object;
+    contents?: Content[];
+    style?: object;
 }
 
-export default function SlideContainer(props: SlideContainer) {
-	return (
-		<Style style={props?.style}>
-			{props?.contents &&
-				(props?.contents?.length > 0 ? (
-					props?.contents.map((content, i) => (
-						<SlideContent key={i} active={content.active} style={content?.style}>
-							{content.children}
-						</SlideContent>
-					))
-				) : (
-					<SlideContent>There is no content.</SlideContent>
-				))}
-		</Style>
-	);
+function SlideContainer(props: SlideContainer) {
+    return (
+        <Style style={props?.style}>
+            {props?.contents &&
+                (props?.contents?.length > 0 ? (
+                    props?.contents.map((content, i) => (
+                        <SlideContent key={i} active={content.active} style={content?.style}>
+                            {content.children}
+                        </SlideContent>
+                    ))
+                ) : (
+                    <SlideContent>There is no content.</SlideContent>
+                ))}
+        </Style>
+    );
 }
+
+export default memo(SlideContainer);
