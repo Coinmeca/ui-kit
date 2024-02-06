30fea018f8bf84e33eee0b9eec009666898c19f8
ZINV <71391363+l-Zetta-l@users.noreply.github.com>
2024-02-07 00:44:57 +0900

    Fix - WindowSize, Theme Hook

--- src/hooks/useTheme.tsx
+++ src/hooks/useTheme.tsx
@@ -1,6 +1,32 @@
-import { useContext } from "react";
-import { ThemeContext } from "contexts/Theme";
+"use client";
+import { useEffect, useState } from "react";
 
-export default function useTheme() {
-    return useContext(ThemeContext);
+export type Mode = "light" | "dark";
+
+export interface Theme {
+    theme: Mode;
+    setTheme: (mode?: Mode) => void;
+}
+
+export default function useTheme(): Theme {
+    const [theme, updateTheme] = useState<Mode | undefined>();
+
+    function setTheme(mode?: Mode) {
+        updateTheme(mode);
+        mode ? localStorage?.setItem("theme", mode) : localStorage?.removeItem("theme");
+    }
+
+    useEffect(() => {
+        function changeTheme(mode: boolean) {
+            if (!localStorage?.getItem("theme")) updateTheme(mode ? "light" : "dark");
+        }
+
+        const theme = window.matchMedia("(prefers-color-scheme: light)") || CSS.supports("color-scheme", "light");
+        theme.addEventListener("theme", (event: any) => changeTheme(event.matches));
+        changeTheme(theme.matches);
+
+        return () => theme.removeEventListener("theme", (event: any) => changeTheme(event.matches));
+    }, []);
+
+    return { theme, setTheme };
 }
--- src/hooks/useWindowSize.tsx
+++ src/hooks/useWindowSize.tsx
@@ -1,6 +1,34 @@
-import { useContext } from "react";
-import { WindowSizeContext } from "contexts/WindowSize";
+"use client";
+import { useState, useEffect } from "react";
 
-export default function useTheme() {
-    return useContext(WindowSizeContext);
+export interface WindowSize {
+    width: number;
+    height: number;
+}
+
+export default function useWindowSize() {
+    const [windowWidth, setWindowWidth] = useState<number>(1920);
+    const [windowHeight, setWindowHeight] = useState<number>(1080);
+    const [windowSize, setWindowSize] = useState<WindowSize>({
+        width: windowWidth,
+        height: windowHeight,
+    });
+
+    useEffect(() => {
+        function windowResize() {
+            if (windowWidth !== window.innerWidth) setWindowWidth(window.innerWidth);
+            if (windowHeight !== window.innerHeight) setWindowHeight(window.innerHeight);
+            setWindowSize({
+                width: window.innerWidth,
+                height: window.innerHeight,
+            });
+        }
+
+        window.addEventListener("resize", windowResize);
+        windowResize();
+
+        return () => window.removeEventListener("resize", windowResize);
+    }, []);
+
+    return { windowSize, windowWidth, windowHeight };
 }
