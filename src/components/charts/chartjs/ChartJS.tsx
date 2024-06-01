4ca8168edcdb220178fa5c4023c5120b470f1282
Zetta <zinv@coinmeca.net>
2024-06-01 21:15:57 +0900

    Update - UIs

--- src/components/charts/chartjs/ChartJS.tsx
+++ src/components/charts/chartjs/ChartJS.tsx
@@ -1,13 +1,14 @@
-"use client";
-import { useState, useEffect, useRef, Suspense, memo } from "react";
-import { Chart } from "chart.js/auto";
+'use client';
+import { Chart } from 'chart.js/auto';
+import { Suspense, memo, useEffect, useRef, useState } from 'react';
 
 export interface ChartJS {
-    type?: "bar" | "line" | "scatter" | "bubble" | "pie" | "doughnut" | "polarArea" | "radar";
+    type?: 'bar' | 'line' | 'scatter' | 'bubble' | 'pie' | 'doughnut' | 'polarArea' | 'radar';
     color?: {
         default?: string;
         theme?: string;
     };
+    padding?: number[] | ''[],
     data?: Data[];
     onHover?: Function;
     fallback?: any;
@@ -22,7 +23,7 @@ export const ChartJS = (props: ChartJS) => {
     const chartRef: any = useRef();
 
     const [data, setData] = useState<any>();
-    const theme = props?.color?.theme && props?.color?.theme === "light" ? "0,0,0" : "255,255,255";
+    const theme = props?.color?.theme && props?.color?.theme === 'light' ? '0,0,0' : '255,255,255';
     const [color, setColor] = useState({
         default: props?.color?.default ? `rgb(${props?.color?.default})` : `rgb(${theme})`,
         theme: {
@@ -34,16 +35,36 @@ export const ChartJS = (props: ChartJS) => {
         },
     });
 
+    const initial = 8;
+    const padding = {
+        top: props?.padding && (typeof props?.padding[0] === 'number' ? props?.padding[0] : initial),
+        right: props?.padding && (
+            props?.padding?.length === 1
+                ? (typeof props?.padding[0] === 'number' ? props?.padding[0] : initial)
+                : (props?.padding?.length > 1 && typeof props?.padding[1] === 'number')
+                    ? props?.padding[1]
+                    : initial),
+        bottom: props?.padding && (
+            props?.padding?.length === 1
+                ? (typeof props?.padding[0] === 'number' ? props?.padding[0] : initial)
+                : (props?.padding?.length > 2 && typeof props?.padding[2] === 'number')
+                    ? props?.padding[2]
+                    : initial),
+        left: props?.padding && (
+            props?.padding?.length === 1
+                ? (typeof props?.padding[0] === 'number' ? props?.padding[0] : initial)
+                : (props?.padding?.length > 3 && typeof props?.padding[3] === 'number')
+                    ? props?.padding[3]
+                    : (props?.padding?.length > 1 && typeof props?.padding[1] === 'number')
+                        ? props?.padding[1]
+                        : initial),
+    };
+
     const options = {
         responsive: true,
         maintainAspectRatio: false,
         layout: {
-            padding: {
-                left: 0,
-                right: 64,
-                top: 16,
-                bottom: 0,
-            },
+            padding,
         },
         scales: {
             x: {
@@ -51,7 +72,7 @@ export const ChartJS = (props: ChartJS) => {
             },
             y: {
                 display: false,
-                // beginAtZero: true
+                beginAtZero: true
             },
         },
         interaction: {
@@ -70,8 +91,8 @@ export const ChartJS = (props: ChartJS) => {
             tooltip: {
                 enabled: true,
                 padding: 0,
-                borderColor: "transparent",
-                backgroundColor: "transparent",
+                borderColor: 'transparent',
+                backgroundColor: 'transparent',
                 callbacks: {
                     // labelPointStyle: (context) => {
                     //     return {
@@ -80,11 +101,11 @@ export const ChartJS = (props: ChartJS) => {
                     //     };
                     // },
                     label: (value: any) => {
-                        if (typeof props?.onHover === "function") props?.onHover(value);
-                        return "";
+                        if (typeof props?.onHover === 'function') props?.onHover(value);
+                        return '';
                     },
                     title: function () {
-                        return "";
+                        return '';
                     },
                 },
             },
@@ -103,7 +124,7 @@ export const ChartJS = (props: ChartJS) => {
                     {
                         data: props?.data?.map((data: Data) => data?.value),
                         borderColor: color.theme.medium,
-                        pointBorderColor: "transparent",
+                        pointBorderColor: 'transparent',
                         pointBackgroundColor: color.theme.strong,
                         pointHoverBackgroundColor: `rgb(${theme})`,
                         pointRadius: 2,
@@ -116,10 +137,10 @@ export const ChartJS = (props: ChartJS) => {
 
     useEffect(() => {
         if (chartRef?.current) {
-            const chart = new Chart(chartRef?.current?.getContext("2d"), {
+            const chart = new Chart(chartRef?.current?.getContext('2d'), {
                 type: props?.type,
-                data: data,
-                options: options,
+                data,
+                options,
             });
             return () => {
                 chart.destroy();
@@ -132,6 +153,6 @@ export const ChartJS = (props: ChartJS) => {
             <canvas ref={chartRef} onMouseLeave={handleMouseLeave} />
         </Suspense>
     );
-}
+};
 
 export default memo(ChartJS);
