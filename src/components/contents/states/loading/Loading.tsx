aadc3ae3d7a7ccc8a5ef832b5f103cadfe1b5469
ZINV <71391363+l-Zetta-l@users.noreply.github.com>
2024-07-27 07:54:07 +0900

    Fix - Process State

--- src/components/contents/states/loading/Loading.tsx
+++ src/components/contents/states/loading/Loading.tsx
@@ -1,10 +1,9 @@
 "use client";
 import State from "components/contents/states/State";
 import type { State as Props } from "components/contents/states/State";
-import image from "../../../../assets/animation/loading.gif";
 
 export default function Loading(props: Props) {
-    const img = props?.img?.src || image;
+    const img = props?.img?.src || require("../../../../assets/animation/loading.gif");
 
     return <State {...props} img={{ src: img }} />;
 }
--- src/components/contents/states/success/Success.tsx
+++ src/components/contents/states/success/Success.tsx
@@ -1,10 +1,9 @@
 "use client";
 import State from "components/contents/states/State";
 import type { State as Props } from "components/contents/states/State";
-import image from "../../../../assets/animation/success.gif";
 
 export default function Success(props: Props) {
-    const img = props?.img?.src || image;
+    const img = props?.img?.src || require("../../../../assets/animation/success.gif");
 
     return <State {...props} img={{ src: img }} />;
 }
