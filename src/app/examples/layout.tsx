61b525dcdcaaa3f403f7449b56a5f762bc44de07
ZINV <71391363+l-Zetta-l@users.noreply.github.com>
2024-08-04 19:32:17 +0900

    Fix - Utils

--- src/app/examples/layout.tsx
+++ src/app/examples/layout.tsx
@@ -6,11 +6,14 @@ export default function ExamplesLayout(props: any) {
     const { header, sidebars, toast } = Dummy();
 
     return (
-        <>
+        <Frames.Frame
+            header={header}
+            sidebar={sidebars}
+            toast={toast}
+            align={"right"}
+            background={{ img: { src: 2 } }}
+            side={56}>
             {props?.children}
-            {/* <Frames.Frame header={header} sidebar={sidebars} toast={toast} align={"right"} background={{ img: { src: 2 } }} side={56}> */}
-            {/* {props?.children} */}
-            {/* </Frames.Frame> */}
-        </>
+        </Frames.Frame>
     );
 }
