08e47492521f5509f448cdfee7d17594abc8da99
ZINV <71391363+l-Zetta-l@users.noreply.github.com>
2024-08-17 19:04:52 +0900

    Update - Types

---
+++ src/types/account.ts
@@ -0,0 +1,30 @@
+﻿import { Asset } from "types";
+
+export interface Volume {
+    amount?: number;
+    value?: number;
+}
+
+export interface Sector {
+    buy?: Volume;
+    sell?: Volume;
+    return?: Volume;
+    returnRate?: Volume;
+}
+
+export interface AccountAsset extends Omit<Asset, 'balance'> {
+    value?: number;
+    pnl?: number;
+    leverage?: number;
+    order?: Volume;
+    balance?: Volume;
+    total?: Sector;
+    average?: Sector;
+    count?: {
+        buy?: number;
+        sell?: number;
+        order?: number;
+        long?: number;
+        short?: number;
+    }
+}
--- src/types/index.ts
+++ src/types/index.ts
@@ -1,3 +1,4 @@
+export * from './account';
 export * from './farm';
 export * from './history';
 export * from './market';
