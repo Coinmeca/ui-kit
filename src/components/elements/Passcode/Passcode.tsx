6af6d8ff846b6afa4c2d455e783221ee9bf93648
ZINV <71391363+l-Zetta-l@users.noreply.github.com>
2024-10-21 21:51:39 +0900

    Update - passcode

--- src/components/elements/Passcode/Passcode.tsx
+++ src/components/elements/Passcode/Passcode.tsx
@@ -1,7 +1,7 @@
 ﻿"use client";
-import Style, { Dot } from './Passcode.styled';
+import Style, { Dot } from "./Passcode.styled";
 
-interface Passcode{
+interface Passcode {
     index: number;
     length: number;
     scale?: number;
@@ -11,20 +11,29 @@ interface Passcode{
     gap?: number | string;
     stroke?: number | string;
     padding?: number;
+    effect?: boolean;
     style?: object;
 }
 
-export default function Passcode(props:Passcode) {
+export default function Passcode(props: Passcode) {
     const scale = props?.scale || 1;
     const size = props?.scale || 1;
     const gap = props?.gap || 2;
     const index = props?.index;
     const length = props?.length;
-    const color = props?.color || 'black';
-    const stroke = props?.stroke || '1px';
+    const color = props?.color || "black";
+    const stroke = props?.stroke || "1px";
     const padding = props?.padding || 2;
+    const effect = props?.effect || false;
 
-    return <Style $scale={scale} $gap={gap} $padding={padding} $color={color} $error={props?.error} style={props?.style}>
-        {length && [...Array(length)].map((_, i: number) => (<Dot key={i} $active={index > i} $size={size} $stroke={stroke}/>))}
-</Style>
+    return (
+        <Style $scale={scale} $gap={gap} $padding={padding} $color={color} $error={props?.error} style={props?.style}>
+            {length &&
+                [...Array(length)].map((_, i: number) => (
+                    <Dot key={i} $active={index > i} $size={size} $stroke={stroke} $effect={effect}>
+                        {effect && <div />}
+                    </Dot>
+                ))}
+        </Style>
+    );
 }
