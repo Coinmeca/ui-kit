875dba677e2ab872640facb1876f69dbe1af5d28
ZINV <71391363+l-Zetta-l@users.noreply.github.com>
2024-10-23 23:56:26 +0900

    Update - slide contents

--- src/components/layouts/contents/slide/SlideContainer.styled.tsx
+++ src/components/layouts/contents/slide/SlideContainer.styled.tsx
@@ -2,19 +2,19 @@
 import { css, styled } from "styled-components";
 import * as Page from "components/layouts/page/Page.styled";
 
-const Style = styled.div<{$vertical?:boolean}>`
+const Style = styled.div<{ $vertical?: boolean }>`
     position: relative;
     display: flex;
     width: 100%;
     height: 100%;
-    min-height: max-content;
+    /* min-height: max-content; */
 
-    ${({ $vertical }) => $vertical && css`
-        flex-direction:column;
-        min-height: initial;
-    `}
-
-    /* ${Page.default} > & {
+    ${({ $vertical }) =>
+        $vertical &&
+        css`
+            flex-direction: column;
+            min-height: initial;
+        `}/* ${Page.default} > & {
         @media (prefers-color-scheme: light) {
             background: rgb(var(--white));
         }
